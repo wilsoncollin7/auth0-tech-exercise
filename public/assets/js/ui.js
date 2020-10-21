921ed73f855d4335ec3acf1710d291a29a0e2bb1
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-10-21 17:48:06 -0400

    made actual menus that change the main section

---
+++ public/assets/js/ui.js
@@ -0,0 +1,11 @@
+$("#applications").on("click", function() {
+  $("#mainHeader").text("Applications");
+});
+
+$("#rules").on("click", function() {
+  $("#mainHeader").text("Rules");
+});
+
+$("#users").on("click", function() {
+  $("#mainHeader").text("Users");
+});
