696f6043a69d7ca20a6e9db3359c2bcfeba8d82e
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-10-21 10:38:58 -0400

    added basic styling

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,9 @@
+const express = require("express");
+const app = express();
 const path = require("path");
 
 module.exports = function(app) {
-  app.get("*", function(req, res) {
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 };
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
+// require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
