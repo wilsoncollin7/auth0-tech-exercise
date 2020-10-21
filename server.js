17ed3e855b43855c66ddf0bb1c8b4e4e0e8cc737
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-10-21 10:03:24 -0400

    added inital file structure

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log(`Now listening on port: ${PORT}`);
+});
