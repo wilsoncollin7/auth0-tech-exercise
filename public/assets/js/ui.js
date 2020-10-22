31fb2babd846ef17488f358c004da79e564cbf53
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-10-22 16:03:50 -0400

    added basic logic to the user and rules button and baisc styling

--- public/assets/js/ui.js
+++ public/assets/js/ui.js
@@ -1,11 +1,11 @@
-$("#applications").on("click", function() {
-  $("#mainHeader").text("Applications");
-});
-
 $("#rules").on("click", function() {
   $("#mainHeader").text("Rules");
+  $(".rules").removeClass("hidden");
+  $(".users").addClass("hidden");
 });
 
 $("#users").on("click", function() {
   $("#mainHeader").text("Users");
+  $(".rules").addClass("hidden");
+  $(".users").removeClass("hidden");
 });
